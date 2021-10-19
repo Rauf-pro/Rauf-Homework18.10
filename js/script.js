060e10393076b6c46961089edb21b483c555000e
Rauf-pro <87373669+Rauf-pro@users.noreply.github.com>
2021-10-19 06:00:43 +0400

    added files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function showmenu() {
+   let show = document.getElementById("top-cart-content");
+   if (show.style.display == "none") {
+    show.style.display = "block";
+  } else {
+    show.style.display = "none";
+  }
+}
+   
+
+
+
